83113e19c87f27b4870d5b2a59d4c39038d62f2d
Heang2470 <sm.heang@usea.edu.kh>
2023-01-04 20:18:47 +0700

    Update

--- en/include/right-content-economic.php
+++ en/include/right-content-economic.php
@@ -1,11 +1,35 @@
 <div class="col-xxl-3">
 				<div class="right-content">
-					<ul>
-						<li><a href="#" class="<?php if($page== 'vision-mission'){echo 'active';}?>">Vision & Mission</a></li><br><hr>
-						<li><a href="#" class="<?php if($page== 'ass'){echo 'active';}?>">Associate's Degree</a></li><br><hr>
-						<li><a href="#" class="<?php if($page== 'bac'){echo 'active';}?>">Bachelor's Degree</a></li><br><hr>
-						<li><a href="#" class="<?php if($page== 'mba'){echo 'active';}?>">Master's Degree</a></li><br><hr>
-					</ul>
+					<div><a href="#">Vision & Mission</a></div>
+					<div>
+						<div class="item"><a href="#">Associate's Degree</a></div>
+						<div class="sub-menu">
+							<div class="sub-item"><a href="#">Accounting</a></div>
+							<div class="sub-item"><a href="#">Finance & Banking</a></div>
+							<div class="sub-item"><a href="#">Management</a></div>
+							<div class="sub-item"><a href="#">Marketing</a></div>
+							<div class="sub-item"><a href="#">Tourism</a></div>
+						</div>
+					</div>
+					<div>
+						<div class="item"><a href="#">Bachelor's Degree</a></div>
+						<div class="sub-menu">
+							<div class="sub-item"><a href="#">Management</a></div>
+							<div class="sub-item"><a href="#">Marketing</a></div>
+							<div class="sub-item"><a href="#">Accounting & Auditing</a></div>
+							<div class="sub-item"><a href="#">Finance & Banking</a></div>
+							<div class="sub-item"><a href="#">Industrail Econnomics</a></div>
+							<div class="sub-item"><a href="#">Intrepreneurship</a></div>
+							<div class="sub-item"><a href="#">Internation Business</a></div>
+							<div class="sub-item"><a href="#">International Business</a></div>
+							<div class="sub-item"><a href="#">International Tourism Management</a></div>
+							<div class="sub-item"><a href="#">Catering Management</a></div>
+							<div class="sub-item"><a href="#">Tourism & Hospitality Management</a></div>
+							<div class="sub-item"><a href="#">Eco-Tourism</a></div>
+							<div class="sub-item"><a href="#">Event Management</a></div>
+						</div>
+					</div>
+					<div class="item"><a href="#">Master's Degree</a></div>
 				</div>
 			</div>
 		</div>
