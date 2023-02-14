dea5fd43a50701928fca058f066b49171d32798e
Heang2470 <sm.heang@usea.edu.kh>
2023-02-14 18:52:50 +0700

    new update 02

--- en/include/right-content-laws.php
+++ en/include/right-content-laws.php
@@ -1,3 +1,4 @@
+
 <div class="col-xxl-3">
 				<div class="right-content-economic">
 					<div class="item"><a class="item-link" href="../vission-Mission/vision-mision-corevalue.php">Vision & Mission</a></div><hr>
@@ -24,7 +25,7 @@
 										<a class="nav-link item-link" href="#"> Bachelor's Degree <i class="fas fa-angle-down"></i> </a>
 										<ul class="submenu collapse">
 											<li><a class="nav-link item-link" href="../BachelorDegree/bachelor-degree-marketing.php"> International Law </a></li>
-											<li><a class="nav-link item-link" href="../BachelorDegree/bachelor-degree-accounting-auditing.php"> Law</a> </li>
+											<li><a class="nav-link item-link" href="faculty-sciences-law.php?id=382"> Law</a> </li>
 											<li><a class="nav-link item-link" href="../BachelorDegree/bachelor-degree-finance-banking.php"> Political Sciences and International Relations </a> </li>
 											<li><a class="nav-link item-link" href="../BachelorDegree/bachelor-degree-industrail-economics.php"> Private Law </a> </li>
 											<li><a class="nav-link item-link" href="../BachelorDegree/bachelor-international-economics.php"> Public Administration </a> </li>
