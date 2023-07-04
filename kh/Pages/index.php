7672b0e74926c0d9ce24f8bfa6870a2ca0a6c7d0
Liheng <122438825+lihengsomkh@users.noreply.github.com>
2023-07-04 16:51:13 +0700

    Kh Event Card

--- kh/Pages/index.php
+++ kh/Pages/index.php
@@ -50,7 +50,7 @@ include_once '../../connection/db.connection.php';
 								<p><?php echo substr($value['event_description_kh'], 0, 100) . "..."; ?></p>
 							</div>
 							<div style="text-align: right;">
-								<a href="#" type="button" class="btn btn-outline-primary"> អានបន្ថែម </a>
+								<a href="#" type="button" class="btn btn-sm event-btn text-end py-1 px-3"> អានបន្ថែម </a>
 							</div>
 						</div>
 					</div>
@@ -63,36 +63,96 @@ include_once '../../connection/db.connection.php';
 			<!-- =====> End Upcoming Events <===== -->
 
 			<!-- =====> Start Events <===== -->
-			<div class="row animate-box mt-5 mb-5" id=" events">
+			<!-- <div class="row animate-box mt-5 mb-5" id=" events">
 				<div class="row animate-box fh5co-heading">
 					<p style="font-size: 20px; font-weight: bold;">
 						<a href="events.php" style="text-decoration: none; color:#002060; font-size: 12pt;"> ព្រឹត្តិការណ៍ | <span style="font-size: 12px; font-weight: 500;">ព្រឹត្តិការណ៍ទាំងអស់</span></a>
 					</p>
 				</div>
 				<?php
-				$stmt = $conn->prepare("SELECT * from usea_events WHERE event_status = 'past' ORDER BY event_date DESC limit 4;");
-				$stmt->execute();
-				$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-				foreach ($result as $key => $value) { ?>
+				//$stmt = $conn->prepare("SELECT * from usea_events WHERE event_status = 'past' ORDER BY event_date DESC limit 4;");
+				//$stmt->execute();
+				//$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+				//foreach ($result as $key => $value) { ?>
 					<div class="col-xxl-3 col-xl-6 col-lg-6 col-md-6 col-sm-12 animate-box d-flex gx-2 gy-2" id="events-card">
 						<div class="card">
 							<div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-								<img src="../../media/events/<?php echo $value['event_cover']; ?>" class="img-fluid " id="card-events"/>
+								<img src="../../media/events/<?php// echo $value['event_cover']; ?>" class="img-fluid " id="card-events"/>
 								<a href="#!">
 									<div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
 								</a>
 							</div>
 							<div class="card-body">
-								<h5 class="card-title" style="font-size: 1rem; color: #002060;"><?php echo substr($value['event_title_kh'], 0, 250) . "..."; ?></h5>
-								<p class="card-text"><?php echo substr($value['event_description_kh'], 0, 0) ?></p>
+								<h5 class="card-title" style="font-size: 1rem; color: #002060;"><?php// echo substr($value['event_title_kh'], 0, 250) . "..."; ?></h5>
+								<p class="card-text"><?php// echo substr($value['event_description_kh'], 0, 0) ?></p>
 							</div>
 							<div style="text-align: right;">
-								<a href="event-detail.php?id=<?php echo $value['event_id'] ?>" class="btn btn-danger btn-sm">អានបន្ថែម</a>
+								<a href="event-detail.php?id=<?php //echo $value['event_id'] ?>" class="btn btn-danger btn-sm">អានបន្ថែម</a>
 							</div>
 						</div>
 					</div>
-				<?php } ?>
-			</div>
+				<?php //} ?>
+			</div> -->
+					<!-- <....................Kh card .....................> -->
+			
+			<div class="row mt-5 mb-5 g-2">
+				<div class="row fh5co-heading">
+					<p style="font-size: 20px; font-weight: bold;">
+						<a href="events.php" style="text-decoration: none; color:#002060; font-size: 12pt;"> ព្រឹត្តិការណ៍ | <span style="font-size: 12px; font-weight: 500;">ព្រឹត្តិការណ៍ទាំងអស់</span></a>
+					</p>
+				</div>
+			
+				<div class="col-sm-12 col-md-6 col-xxl-3 mb-sm-2 mb-lg-2 mb-xxl-0">
+					<div class="card w-50 h-100 w-100 animate-card">
+						<img src="../../media/Christmas Day 25-12-20132_s.jpg" alt="aquare" class="card-img-top">
+						<div class="card-body">
+							<h5 class="card-title">ពិធីសំណេះសំណាលជាមួយបុគ្គលិក សាស្ត្រាចារ្យ និងនិស្សិ�...</h5>
+							<p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum eligendi cumque amet veniam a molestiae voluptatem, provident minus porro harum, laboriosam corrupti molestias consequuntur magnam, rerum unde error earum explicabo?</p>
+						</div>
+						<div class="card-footer text-end">
+							<button class="btn btn-sm event-btn text-end py-1 px-3">អានបន្ថែម</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-12 col-md-6 col-xxl-3 mb-sm-2 mb-lg-2 mb-xxl-0">
+					<div class="card w-50 h-100 w-100 animate-card">
+						<img src="../../media/Christmas Day 25-12-20131.JPG" alt="aquare" class="card-img-top">
+						<div class="card-body">
+							<h5 class="card-title">ពិធីសំណេះសំណាលជាមួយបុគ្គលិក សាស្ត្រាចារ្យ និងនិស្សិ�...</h5>
+							<p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum eligendi cumque amet veniam a molestiae voluptatem, provident minus porro harum, laboriosam corrupti molestias consequuntur magnam, rerum unde error earum explicabo?</p>
+						</div>
+						<div class="card-footer text-end">
+							<button class="btn btn-sm event-btn text-end py-1 px-3">អានបន្ថែម</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-12 col-md-6 col-xxl-3 mb-sm-2 mb-lg-2 mb-xxl-0">
+					<div class="card w-50 h-100 w-100 animate-card">
+						<img src="../../media/test.png" alt="aquare" class="card-img-top">
+						<div class="card-body">
+							<h5 class="card-title">ពិធីសំណេះសំណាលជាមួយបុគ្គលិក សាស្ត្រាចារ្យ និងនិស្សិ�...</h5>
+							<p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum eligendi cumque amet veniam a molestiae voluptatem, provident minus porro harum, laboriosam corrupti molestias consequuntur magnam, rerum unde error earum explicabo?</p>
+						</div>
+						<div class="card-footer text-end">
+							<button class="btn btn-sm event-btn text-end py-1 px-3">អានបន្ថែម</button>
+						</div>
+				</div>
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-xxl-3 mb-sm-2 mb-lg-2 mb-xxl-0">
+                <div class="card w-50 h-100 w-100 animate-card">
+                    <img src="../../media/test.png" alt="aquare" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">ពិធីសំណេះសំណាលជាមួយបុគ្គលិក សាស្ត្រាចារ្យ និងនិស្សិ�...</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum eligendi cumque amet veniam a molestiae voluptatem, provident minus porro harum, laboriosam corrupti molestias consequuntur magnam, rerum unde error earum explicabo?</p>
+                    </div>
+                    <div class="card-footer text-end">
+                        <button class="btn btn-sm event-btn text-end py-1 px-3">អានបន្ថែម</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 			<hr style="color: #002060;">
 			<!-- =====> End Events <===== -->
 		</div>
