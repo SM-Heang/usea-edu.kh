7e59d655da94a780b35ea3cb9de1d09432123a63
SM-Heang <mheang67@gmail.com>
2023-08-03 17:52:01 +0700

    update faq design

--- en/Pages/faq.php
+++ en/Pages/faq.php
@@ -43,100 +43,37 @@
                     $sql = "SELECT * FROM usea_faq WHERE keyword = 'general'";
                     $stmt= $conn->prepare($sql);
                     $stmt->execute();
-                    $faq = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    $faq = $stmt->fetchALL(PDO::FETCH_ASSOC);
                     // var_dump($faq);
                 ?>
 				<div id="content-detail">
 					<div class="content-history-detail mt-2">
                         <h2>Question About USEA</h2>
                         <span>General</span>
-                        
-                        <div class="accordion" id="accordionExample">
+                        <?php 
+                            $sql = "SELECT * FROM usea_faq WHERE keyword = 'general'";
+                            $stmt= $conn->prepare($sql);
+                            $stmt->execute();
+                            $faq = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                        ?>
+                        <div class="accordion" id="accordionPanelsStayOpenExample">
                             <?php
-                            $i=0;
-                            foreach ($faq as $key => $value) {
-                                if($i==0){
-
+                                foreach($faq as $key => $value){  
                             ?>
                             <div class="accordion-item">
-                                <h2 class="accordion-header" id="heading<?php $value['faq_id'] ?>">
-                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php $value['faq_id'] ?>" aria-expanded="true" aria-controls="collapse<?php $value['faq_id'] ?>">
-                                    <?php echo $value['faq_question'];?>
+                                <h2 class="accordion-header" id="panelsStayOpen-heading<?php echo $value['faq_id']; ?>">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse<?php echo $value['faq_id']; ?>" aria-expanded="false" aria-controls="panelsStayOpen-collapse<?php echo $value['faq_id']; ?>">
+                                    <?php  echo $value['faq_question']; ?>
                                 </button>
                                 </h2>
-                                <div id="collapse<?php $value['faq_id'] ?>" class="accordion-collapse collapse show" aria-labelledby="heading<?php $value['faq_id'] ?>" data-bs-parent="#accordionExample">
-                                <div class="accordion-body">
-                                <?php echo $value['faq_answer'];?>
-                                </div>
-                                </div>
-                            </div>
-                            <?php }else{ ?>
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="headingTwo">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                    តើខាងសាកលវិទ្យាល័យ មានអាហារូបករណ៍សម្រាប់និស្សិតដែរឬ ទេ?
-                                </button>
-                                </h2>
-                                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                                <div class="accordion-body">
-                                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                                </div>
+                                <div id="panelsStayOpen-collapse<?php echo $value['faq_id']; ?>" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-heading<?php echo $value['faq_id']; ?>">
+                                    <div class="accordion-body">
+                                        <?php echo $value['faq_answer']; ?>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="headingThree">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                    តើសាកលវិទ្យាល័យមាន ជំនាញអ្វីខ្លះ? តើជំនាញនីមួយៗត្រូវចំណាយពេលសិក្សាប៉ុន្មានឆ្នាំ?
-                                </button>
-                                </h2>
-                                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                                <div class="accordion-body">
-                                    <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                                </div>
-                                </div>
-                            </div>
-                            <?php }} ?>
+                            <?php } ?>
                         </div>
-
-                        <!-- <span>Faculty</span>
-                        <div class="accordion" id="accordionExample">
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="headingFour">
-                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                                    ដើម្បីចុះឈ្មោះចូលរៀន តើសាកលវិទ្យាល័យតម្រូវឲ្យភ្ជាប់ឯកសារអ្វីខ្លះ?
-                                </button>
-                                </h2>
-                                <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
-                                <div class="accordion-body">
-                                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                                </div>
-                                </div>
-                            </div>
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="headingTwo">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                    តើខាងសាកលវិទ្យាល័យ មានអាហារូបករណ៍សម្រាប់និស្សិតដែរឬ ទេ?
-                                </button>
-                                </h2>
-                                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                                <div class="accordion-body">
-                                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                                </div>
-                                </div>
-                            </div>
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="headingThree">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                    តើសាកលវិទ្យាល័យមាន ជំនាញអ្វីខ្លះ? តើជំនាញនីមួយៗត្រូវចំណាយពេលសិក្សាប៉ុន្មានឆ្នាំ?
-                                </button>
-                                </h2>
-                                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                                <div class="accordion-body">
-                                    <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                                </div>
-                                </div>
-                            </div>
-                        </div> -->
 					</div>
 				</div>
 			</div>
